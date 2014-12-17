c3825388c5c9dc05670f5624b68eecb6cae2a921
JeroenSormani <jeroen.sormani@gmail.com>
2014-12-17 17:56:02 +0100

    Fix #2 - Check if WooCommerce is active

--- woocommerce-simple-registration.php
+++ woocommerce-simple-registration.php
@@ -60,6 +60,17 @@ class WooCommerce_Simple_Registration {
 	 */
 	public function __construct() {
 
+		// Check if WooCommerce is active
+		if ( ! function_exists( 'is_plugin_active_for_network' ) ) {
+			require_once( ABSPATH . '/wp-admin/includes/plugin.php' );
+		}
+
+		if ( ! in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+			if ( ! is_plugin_active_for_network( 'woocommerce/woocommerce.php' ) ) {
+				return;
+			}
+		}
+
 		// Initialize plugin parts
 		$this->init();
 
