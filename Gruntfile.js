1f464988fab8da683b406ad8d0804cb0e0a640a2
David <david@shellcreeper.com>
2017-03-22 14:20:26 +0700

    Add Grunt Checktextdomain + Fix text domain #10

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,48 @@
 'use strict';
 module.exports = function(grunt) {
 
-  grunt.initConfig({
-    makepot: {
-      reviews: {
-        options: {
-          type: 'wp-plugin'
-        }
-      }
-    }
-  });
+	grunt.initConfig({
+		makepot: {
+			reviews: {
+				options: {
+					type: 'wp-plugin'
+				}
+			}
+		},
+		checktextdomain: {
+			standard: {
+				options:{
+					force: true,
+					text_domain: 'woocommerce-simple-registration',
+					create_report_file: false,
+					correct_domain: true,
+					keywords: [
+						'__:1,2d',
+						'_e:1,2d',
+						'_x:1,2c,3d',
+						'esc_html__:1,2d',
+						'esc_html_e:1,2d',
+						'esc_html_x:1,2c,3d',
+						'esc_attr__:1,2d', 
+						'esc_attr_e:1,2d', 
+						'esc_attr_x:1,2c,3d', 
+						'_ex:1,2c,3d',
+						'_n:1,2,4d', 
+						'_nx:1,2,4c,5d',
+						'_n_noop:1,2,3d',
+						'_nx_noop:1,2,3c,4d'
+					]
+				},
+				files: [{
+					src: ['**/*.php','!node_modules/**'],
+					expand: true,
+				}],
+			},
+		},
+	});
 
-  grunt.loadNpmTasks( 'grunt-wp-i18n' );
+	grunt.loadNpmTasks( 'grunt-wp-i18n' );
+	grunt.loadNpmTasks( 'grunt-checktextdomain' );
 
-  grunt.registerTask( 'pot', [ 'makepot' ] );
+	grunt.registerTask( 'pot', [ 'makepot' ] );
 };
--- templates/registration-form.php
+++ templates/registration-form.php
@@ -14,7 +14,7 @@ wp_enqueue_script( 'wc-password-strength-meter' );
 
 	<?php wc_print_notices(); ?>
 
-	<h2><?php _e( 'Register', 'woocommerce' ); ?></h2>
+	<h2><?php _e( 'Register', 'woocommerce-simple-registration' ); ?></h2>
 
 	<form method="post" class="register">
 
@@ -23,35 +23,35 @@ wp_enqueue_script( 'wc-password-strength-meter' );
 		<?php if ( 'no' === get_option( 'woocommerce_registration_generate_username' ) ) : ?>
 
 			<p class="woocommerce-FormRow woocommerce-FormRow--wide form-row form-row-wide">
-				<label for="reg_username"><?php _e( 'Username', 'woocommerce' ); ?> <span class="required">*</span></label>
+				<label for="reg_username"><?php _e( 'Username', 'woocommerce-simple-registration' ); ?> <span class="required">*</span></label>
 				<input type="text" class="woocommerce-Input woocommerce-Input--text input-text" name="username" id="reg_username" value="<?php if ( ! empty( $_POST['username'] ) ) echo esc_attr( $_POST['username'] ); ?>" />
 			</p>
 
 		<?php endif; ?>
 
 		<p class="woocommerce-FormRow woocommerce-FormRow--wide form-row form-row-wide">
-			<label for="reg_email"><?php _e( 'Email address', 'woocommerce' ); ?> <span class="required">*</span></label>
+			<label for="reg_email"><?php _e( 'Email address', 'woocommerce-simple-registration' ); ?> <span class="required">*</span></label>
 			<input type="email" class="woocommerce-Input woocommerce-Input--text input-text" name="email" id="reg_email" value="<?php if ( ! empty( $_POST['email'] ) ) echo esc_attr( $_POST['email'] ); ?>" />
 		</p>
 
 		<?php if ( 'no' === get_option( 'woocommerce_registration_generate_password' ) ) : ?>
 
 			<p class="woocommerce-FormRow woocommerce-FormRow--wide form-row form-row-wide">
-				<label for="reg_password"><?php _e( 'Password', 'woocommerce' ); ?> <span class="required">*</span></label>
+				<label for="reg_password"><?php _e( 'Password', 'woocommerce-simple-registration' ); ?> <span class="required">*</span></label>
 				<input type="password" class="woocommerce-Input woocommerce-Input--text input-text" name="password" id="reg_password" />
 			</p>
 
 		<?php endif; ?>
 
 		<!-- Spam Trap -->
-		<div style="<?php echo ( ( is_rtl() ) ? 'right' : 'left' ); ?>: -999em; position: absolute;"><label for="trap"><?php _e( 'Anti-spam', 'woocommerce' ); ?></label><input type="text" name="email_2" id="trap" tabindex="-1" /></div>
+		<div style="<?php echo ( ( is_rtl() ) ? 'right' : 'left' ); ?>: -999em; position: absolute;"><label for="trap"><?php _e( 'Anti-spam', 'woocommerce-simple-registration' ); ?></label><input type="text" name="email_2" id="trap" tabindex="-1" /></div>
 
 		<?php do_action( 'woocommerce_register_form' ); ?>
 		<?php do_action( 'register_form' ); ?>
 
 		<p class="woocomerce-FormRow form-row">
 			<?php wp_nonce_field( 'woocommerce-register', 'woocommerce-register-nonce' ); ?>
-			<input type="submit" class="woocommerce-Button button" name="register" value="<?php esc_attr_e( 'Register', 'woocommerce' ); ?>" />
+			<input type="submit" class="woocommerce-Button button" name="register" value="<?php esc_attr_e( 'Register', 'woocommerce-simple-registration' ); ?>" />
 		</p>
 
 		<?php do_action( 'woocommerce_register_form_end' ); ?>
