53b8c25852602e318dca52211ba5fb13753619e3
David <david@shellcreeper.com>
2017-08-23 20:48:48 +0700

    Add Login Link Below Form #17

--- templates/registration-form.php
+++ templates/registration-form.php
@@ -54,6 +54,11 @@ wp_enqueue_script( 'wc-password-strength-meter' );
 			<input type="submit" class="woocommerce-Button button" name="register" value="<?php esc_attr_e( 'Register', 'woocommerce-simple-registration' ); ?>" />
 		</p>
 
+		<p class="forgot-password">
+			<a href="<?php echo esc_url( wp_login_url( get_permalink() ) ); ?>"><?php esc_html_e( 'Login', 'woocommerce-simple-registration' ); ?></a>&nbsp;|&nbsp;
+			<a href="<?php echo esc_url( wp_lostpassword_url() ); ?>"><?php esc_html_e( 'Lost your password?', 'woocommerce-simple-registration' ); ?></a>
+		</p>
+
 		<?php do_action( 'woocommerce_register_form_end' ); ?>
 
 	</form>
